2c8dec319de51fed624b6a683cf56502560d24fa
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-11-18 19:26:27 +0530

    added basic functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-const canvas : HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+import { GenerateNumbers } from "./functions";
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// Variables
+const canvas : HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx : CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-ctx.rect(50, 50, canvas.width/2, canvas.height/2);
+// Setting width height of canvas
+canvas.width = window.innerWidth / 2;
+canvas.height = window.innerHeight / 2;
+canvas.style.cssText = 'background : black';
+
+// Generating random numbers on clicking the button
+const randomNumbersBtn : HTMLElement = document.querySelector(".randomNumbersBtn");
+const numbersArea : HTMLElement = document.querySelector(".numbers");
+numbersArea.style.cssText = 'background : white;';
+
+randomNumbersBtn.addEventListener('click', () => {
+    console.log("This should be working");
+    const arr = GenerateNumbers();
+
+    alert(arr);
+    // let inner = "";
+    // for (let i = 0; i < arr.length; i++) {
+    //     inner += arr[i] + ' ';
+    // }
+
+    // numbersArea.innerText = inner;
+})
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,26 @@
+// "use strict";
+// exports.__esModule = true;
+// var functions_1 = require("./functions");
+
+import { GenerateNumbers } from "/static/js/functions.js";
+// Variables
 var canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 var ctx = canvas.getContext('2d');
-ctx.rect(50, 50, canvas.width / 2, canvas.height / 2);
+// Setting width height of canvas
+canvas.width = window.innerWidth / 2;
+canvas.height = window.innerHeight / 2;
+canvas.style.cssText = 'background : black';
+// Generating random numbers on clicking the button
+var randomNumbersBtn = document.querySelector(".randomNumbersBtn");
+var numbersArea = document.querySelector(".numbers");
+numbersArea.style.cssText = 'background : white;';
+randomNumbersBtn.addEventListener('click', function () {
+    console.log("This should be working");
+    var arr = (0, GenerateNumbers)();
+    alert(arr);
+    // let inner = "";
+    // for (let i = 0; i < arr.length; i++) {
+    //     inner += arr[i] + ' ';
+    // }
+    // numbersArea.innerText = inner;
+});
